d4f7f8b83c838a14462f936368f8f3b18e6c7d5e
Alorr-jpn <corasonakoachere@gmail.com>
2023-08-17 13:25:39 +0100

    blackjack game counting cards usinf javascript

---
+++ countingCard.js
@@ -0,0 +1,24 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  const deck =[[2, 3, 4, 5, 6],[7, 8, 9],[10,'J', 'Q', 'K', 'A']];
+  let decision = "Hold"
+  if(deck[0].includes(card)){
+    count += 1;
+  } 
+  if (deck[1].includes(card)){
+    count = count;
+  }
+  if (deck[2].includes(card)){
+    count -=1;
+  }  
+
+ if(count > 0)
+      decision = "Bet"
+
+  return count + " " + decision
+  // Only change code above this line
+}
+
+cc(3); cc(2); cc('A'); cc(10); cc('K');
